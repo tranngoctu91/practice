6059a0f591e986047ff9c0ab215ffa098bdde912
Tran Ngoc Tu <ngoctu17101991@gmail.com>
2024-07-15 10:31:31 +0700

    update

--- demo1/my-app/src/Tictactoe/Game.js
+++ demo1/my-app/src/Tictactoe/Game.js
@@ -1,26 +1,62 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 
 import "./GameStyle.css";
 import Board from "./Board";
 import { CheckWin } from "../Helper";
+import { type } from "@testing-library/user-event/dist/type";
 
-const Game = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-  const winder = CheckWin(board);
-  const handleClick = (index) => {
-    const boardCopy = [...board];
-    if (winder || boardCopy[index]) {
-      return;
+const initialState = {
+  board: Array(9).fill(),
+  xIsNext: true,
+};
+
+const gameReducer = (state, action) => {
+  switch (action.type) {
+    case "CLICK": {
+      const { board, xIsNext } = state;
+      const { index, winner } = action.payload;
+      if (winner || board[index]) {
+        return state;
+      }
+      const nextState = JSON.parse(JSON.stringify(state));
+      nextState.board[index] = xIsNext ? "X" : "O";
+      nextState.xIsNext = !xIsNext;
+      return nextState;
     }
-    boardCopy[index] = xIsNext ? "X" : "O";
-    setBoard(boardCopy);
-    setXIsNext(!xIsNext);
+    case "RESEST": {
+      const nextState = JSON.parse(JSON.stringify(state));
+      nextState.board = Array(9).fill(null);
+      nextState.xIsNext = true;
+      return nextState;
+    }
+    default:
+      break;
+  }
+  return state;
+};
+const Game = () => {
+  const [state, dispatch] = useReducer(gameReducer, initialState);
+  const winner = CheckWin(state.board);
+
+  const handleClick = (index) => {
+    dispatch({
+      type: "CLICK",
+      payload: {
+        index,
+        winner,
+      },
+    });
+  };
+  const handleReset = () => {
+    dispatch({
+      type: "RESEST",
+    });
   };
   return (
     <div>
-      <Board cells={board} onClick={handleClick}></Board>
-      {winder ? `winner is ${xIsNext ? "O" : "X"}` : ""}
+      <Board cells={state.board} onClick={handleClick}></Board>
+      {winner ? `winner is ${state.xIsNext ? "O" : "X"}` : ""}
+      <button onClick={() => handleReset()}> Resest</button>
     </div>
   );
 };
