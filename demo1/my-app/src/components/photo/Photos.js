9d484572ae7bb25280b2db3390aa52540e5e328c
Tran Ngoc Tu <ngoctu17101991@gmail.com>
2024-07-23 23:04:13 +0700

    picsum photos

--- demo1/my-app/src/components/photo/Photos.js
+++ demo1/my-app/src/components/photo/Photos.js
@@ -1,12 +1,37 @@
-import React, { useEffect } from "react";
-// https://picsum.photos/v2/list
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+const GetPhotos = async (page) => {
+  try {
+    const s = await axios.get(
+      `https://picsum.photos/v2/list?limit=5&page=${page}`
+    );
+    return s.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const Photos = () => {
-  console.log("outside");
+  const [photos, setPhotos] = useState([]);
+  const [nextPage, setNextPage] = useState(1);
+
   useEffect(() => {
-    document.title = "welcome to use effect";
-    console.log("inside");
-  }, []);
-  return <div>Photo</div>;
+    GetPhotos(nextPage).then((images) => {
+      const newPhotos = [...photos, ...images];
+      setPhotos(newPhotos);
+    });
+  }, [nextPage]);
+
+  const handleLoadmore = () => {
+    setNextPage((nextPage) => nextPage + 1);
+  };
+
+  return (
+    <div className="grid grid-cols-4 gap-5 p-5">
+      {photos.length > 0 && photos.map((s, i) => <p key={i}>{s.id}</p>)}
+      <button onClick={() => handleLoadmore()}>Load more</button>
+    </div>
+  );
 };
 
 export default Photos;
