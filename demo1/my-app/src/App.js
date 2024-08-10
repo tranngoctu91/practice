fddf7b2cfeda372f1c9bf2e1e21f5274aa9ed3bf
Tran Ngoc Tu <ngoctu17101991@gmail.com>
2024-08-10 10:30:00 +0700

    pure main

--- demo1/my-app/src/App.js
+++ demo1/my-app/src/App.js
@@ -2,15 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const name = "Start react";
-  function fullName(firstName, lastName) {
-    return `${firstName} - ${lastName}`;
-  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello {fullName("tu", "tran")}</h1>
+        <h1>Hello</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
